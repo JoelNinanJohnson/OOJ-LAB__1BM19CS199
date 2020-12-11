620f8c9de8ad802f2c1f0f7f591a5c30d9d6ad9b
joel ninan johnson <joel.cs19@bmsce.ac.in>
2020-12-11 14:46:27 +0530

    -

---
+++ LAB 8/WrongAge.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+
+public class WrongAge extends Exception {
+	int a;
+	WrongAge(int a){
+		this.a = a;		
+	}
+	public String toString(){
+		return "WRONG INPUT AGE "+a;
+	}
+}
